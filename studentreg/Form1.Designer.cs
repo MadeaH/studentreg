82d2ce5e1791b1605b7772b3c5b8ac114ea1fbb2
Desktop <Desktop@MADEAD>
2025-04-13 15:27:51 +0800

    Add project files.

---
+++ studentreg/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace studentreg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.LoginLabel = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ignLabel = new System.Windows.Forms.Label();
+            this.nameInput = new System.Windows.Forms.TextBox();
+            this.pwInput = new System.Windows.Forms.TextBox();
+            this.pwLabel = new System.Windows.Forms.Label();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.forgotPassButton = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.panel1.Controls.Add(this.LoginLabel);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(523, 73);
+            this.panel1.TabIndex = 0;
+            // 
+            // LoginLabel
+            // 
+            this.LoginLabel.AutoSize = true;
+            this.LoginLabel.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoginLabel.ForeColor = System.Drawing.Color.White;
+            this.LoginLabel.Location = new System.Drawing.Point(79, 21);
+            this.LoginLabel.Name = "LoginLabel";
+            this.LoginLabel.Size = new System.Drawing.Size(196, 32);
+            this.LoginLabel.TabIndex = 2;
+            this.LoginLabel.Text = "System Login";
+            this.LoginLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::studentreg.Properties.Resources.CVSU_LOGO;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(70, 67);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ignLabel
+            // 
+            this.ignLabel.AutoSize = true;
+            this.ignLabel.BackColor = System.Drawing.Color.Transparent;
+            this.ignLabel.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ignLabel.ForeColor = System.Drawing.Color.White;
+            this.ignLabel.Location = new System.Drawing.Point(196, 127);
+            this.ignLabel.Name = "ignLabel";
+            this.ignLabel.Size = new System.Drawing.Size(71, 16);
+            this.ignLabel.TabIndex = 3;
+            this.ignLabel.Text = "Username";
+            this.ignLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // nameInput
+            // 
+            this.nameInput.Location = new System.Drawing.Point(199, 146);
+            this.nameInput.Name = "nameInput";
+            this.nameInput.Size = new System.Drawing.Size(121, 20);
+            this.nameInput.TabIndex = 4;
+            // 
+            // pwInput
+            // 
+            this.pwInput.Location = new System.Drawing.Point(199, 201);
+            this.pwInput.Name = "pwInput";
+            this.pwInput.PasswordChar = '*';
+            this.pwInput.Size = new System.Drawing.Size(121, 20);
+            this.pwInput.TabIndex = 6;
+            // 
+            // pwLabel
+            // 
+            this.pwLabel.AutoSize = true;
+            this.pwLabel.BackColor = System.Drawing.Color.Transparent;
+            this.pwLabel.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pwLabel.ForeColor = System.Drawing.Color.White;
+            this.pwLabel.Location = new System.Drawing.Point(196, 182);
+            this.pwLabel.Name = "pwLabel";
+            this.pwLabel.Size = new System.Drawing.Size(68, 16);
+            this.pwLabel.TabIndex = 5;
+            this.pwLabel.Text = "Password";
+            this.pwLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // loginButton
+            // 
+            this.loginButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.loginButton.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginButton.ForeColor = System.Drawing.Color.White;
+            this.loginButton.Location = new System.Drawing.Point(199, 275);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(121, 28);
+            this.loginButton.TabIndex = 7;
+            this.loginButton.Text = "LOG IN";
+            this.loginButton.UseVisualStyleBackColor = false;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // forgotPassButton
+            // 
+            this.forgotPassButton.BackColor = System.Drawing.Color.White;
+            this.forgotPassButton.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.forgotPassButton.ForeColor = System.Drawing.Color.Black;
+            this.forgotPassButton.Location = new System.Drawing.Point(199, 246);
+            this.forgotPassButton.Name = "forgotPassButton";
+            this.forgotPassButton.Size = new System.Drawing.Size(121, 28);
+            this.forgotPassButton.TabIndex = 8;
+            this.forgotPassButton.Text = "Forgot Password";
+            this.forgotPassButton.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::studentreg.Properties.Resources.background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(523, 372);
+            this.Controls.Add(this.forgotPassButton);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.pwInput);
+            this.Controls.Add(this.pwLabel);
+            this.Controls.Add(this.nameInput);
+            this.Controls.Add(this.ignLabel);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "[CVSU] Login";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label LoginLabel;
+        private System.Windows.Forms.Label ignLabel;
+        private System.Windows.Forms.TextBox nameInput;
+        private System.Windows.Forms.TextBox pwInput;
+        private System.Windows.Forms.Label pwLabel;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Button forgotPassButton;
+    }
+}
+
